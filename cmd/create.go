9b4bcaf92af6c0db44455304a9010189cacc033d
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2022-01-24 12:32:03 +0530

    Added command description

--- cmd/create.go
+++ cmd/create.go
@@ -20,13 +20,8 @@ import (
 // createCmd represents the create command
 var createCmd = &cobra.Command{
 	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "hms_cli guest create",
+	Long:  `Add guest data by adding flags --check-in="" --check-out="" --name=""`,
 	Run: func(cmd *cobra.Command, args []string) {
 		createGuest()
 	},
@@ -44,7 +39,7 @@ func init() {
 	createCmd.MarkFlagRequired("check-out")
 }
 
-func createGuest()  {
+func createGuest() {
 	url := "http://localhost:4000/guest"
 	_, checkInErr := time.Parse("2006-01-02", guest.CheckInDate)
 	if checkInErr != nil {
